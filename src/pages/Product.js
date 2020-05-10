76a6dae2929827827afda92ef6fa58b3a5cd1b43
Valenza <matheusvvalenza@gmail.com>
2020-05-10 00:50:03 -0300

    added product detail structure

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 export default function Product(props) {
   return (
-    <section className="product">
-      Product {props.match.params.productId}
-    </section>
+    <div className="container">
+      <section className="product">
+        <figure>
+          <img src="https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912" alt=""/>
+        </figure>
+        <div className="product__detail">
+          PRODUCT DETAIL
+        </div>
+      </section>
+    </div>
   )
 };
