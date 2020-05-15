f706e65066db2cde3d9593d413f1738b1de38685
Valenza <matheusvvalenza@gmail.com>
2020-05-15 19:49:32 -0300

    added header icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,15 @@ export default function Header() {
     <header className="header">
       <div className="container">
         <div className="header__logo">
-          <img src={logo} alt="Logo Fashioninsta"/>
+          <img src={logo}  alt="Logo Fashioninsta"/>
         </div>
-        <div className="header__buttons">
-          BOTÕES
+        <div className="header__right">
+          <div className="header__icon">
+            <i className="fa fa-search"></i>
+          </div>
+          <div className="header__icon">
+            <i className="fa fa-shopping-cart"></i>
+          </div>
         </div>
       </div>
     </header>
