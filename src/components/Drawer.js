ca283ea0479048ce10c7762d6bbf0928f58fbf12
Valenza <matheusvvalenza@gmail.com>
2020-05-24 21:33:34 -0300

    added new layout to drawer component

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 export default function Drawer({ active, close }) {
-  const classes = `drawer ${active ? 'drawer--active' : ''}`;
+  const classes = `drawer ${active ? 'drawer--active' : 'drawer--hidden'}`;
 
   return (
     <div className={classes}>
       <div className="drawer__title">
         <i className="fa fa-arrow-left" onClick={close}></i>
+        <span>Sua sacola</span>
       </div>
-      <div className="drawer__content">CONTENT</div>
-      <div className="drawer__footer">FOOTER</div>
+      <div className="drawer__content">
+        <span>Sua sacola está vazia :(</span>
+      </div>
+      <div className="drawer__footer">Subtotal - R$ 0,00</div>
     </div>
   );
 }
