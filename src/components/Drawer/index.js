7553c93b36c63792d3a9345f8e2164ade0879ced
Valenza <matheusvvalenza@gmail.com>
2020-05-31 21:20:05 -0300

    added layout to product in cart

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -6,7 +6,7 @@ export default function Drawer({ children, active, close, title }) {
   return (
     <div className={`drawer ${active ? 'drawer--active' : 'drawer--hidden'}`}>
       <div className="drawer__title">
-        <i className="fa fa-arrow-left" onClick={close}></i>
+        <i className="fa fa-times" onClick={close}></i>
         <span>{title}</span>
       </div>
       {children}
--- src/pages/components/Cart/index.js
+++ src/pages/components/Cart/index.js
@@ -20,12 +20,41 @@ export default function Cart() {
       close={() => dispatch(toggleShowCart())}
     >
       {hasProductsInCart ? (
-        <div className="cart">CART</div>
+        <section className="cart">
+          <div className="cart__content">
+            <article className="chosed-product">
+              <div className="chosed-product__row">
+                <img
+                  className="chosed-product__image"
+                  src="https://viniciusvinna.netlify.app/assets/api-fashionista/20002570_002_catalog_1.jpg"
+                  alt=""
+                />
+                <div className="chosed-product__info">
+                  <p className="cart__product-title">REGATA ALCINHA FOLK</p>
+                  <p className="cart__product-subtitle">Tam.: PP</p>
+                  <div className="change-quantity">
+                    <button className="change-quantity__button">-</button>
+                    <span className="change-quantity__current">0</span>
+                    <button className="change-quantity__button">+</button>
+                  </div>
+                </div>
+                <div className="chosed-product__price">
+                  <p className="cart__product-title">R$ 99,90</p>
+                  <p className="cart__product-subtitle">3 x R$ 33,00</p>
+                </div>
+              </div>
+              <div className="chosed-product__row">
+                <button className="cart__remove">Remover item</button>
+              </div>
+            </article>
+          </div>
+        </section>
       ) : (
-        <div className="cart--empty">
+        <section className="cart--empty">
           <p>Sua sacola está vazia :(</p>
-        </div>
+        </section>
       )}
+      <section className="cart__footer">Subtotal: R$ 0,00</section>
     </Drawer>
   );
 }
