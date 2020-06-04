c71a512bb05622f4f6152ac2896111f1e9a62dd3
Valenza <matheusvvalenza@gmail.com>
2020-06-03 22:02:00 -0300

    turn cart products dinamic according to redux store

--- src/pages/components/Cart/index.js
+++ src/pages/components/Cart/index.js
@@ -5,48 +5,91 @@ import './style.css';
 
 import { toggleShowCart } from 'store/actions/cart';
 
-import { Drawer } from 'components';
+import { Drawer, ImagePlaceholder } from 'components';
 
 export default function Cart() {
   const dispatch = useDispatch();
-  const { showCart } = useSelector((state) => state.cart);
+  const { showCart, cartItems } = useSelector((state) => state.cart);
+  const cartLength = cartItems.length;
+  const hasProductsInCart = cartLength > 0;
 
-  const hasProductsInCart = true;
+  const handleRemoveItem = (selectedSku) => {
+    console.log('handleRemoveItem: ', selectedSku);
+  };
+
+  const handleSubtractItem = (selectedSku) => {
+    console.log('handleSubtractItem: ', selectedSku);
+  };
+
+  const handleAddItem = (selectedSku) => {
+    console.log('handleAddItem: ', selectedSku);
+  };
 
   return (
     <Drawer
-      title="Sua sacola (1)"
+      title={`Sua sacola (${cartLength})`}
       active={showCart}
       close={() => dispatch(toggleShowCart())}
     >
       {hasProductsInCart ? (
         <section className="cart">
           <div className="cart__content">
-            <article className="chosed-product">
-              <div className="chosed-product__row">
-                <img
-                  className="chosed-product__image"
-                  src="https://viniciusvinna.netlify.app/assets/api-fashionista/20002570_002_catalog_1.jpg"
-                  alt=""
-                />
-                <div className="chosed-product__info">
-                  <p className="cart__product-title">REGATA ALCINHA FOLK</p>
-                  <p className="cart__product-subtitle">Tam.: PP</p>
-                  <div className="change-quantity">
-                    <button className="change-quantity__button">-</button>
-                    <span className="change-quantity__current">0</span>
-                    <button className="change-quantity__button">+</button>
+            {cartItems.map((cartItem) => {
+              const {
+                name,
+                actual_price,
+                installments,
+                image,
+                quantity,
+                selectedSku
+              } = cartItem;
+              const size = cartItem.sizes.find(
+                ({ sku }) => sku === cartItem.selectedSku
+              ).size;
+
+              return (
+                <article className="cart-item" key={cartItem.selectedSku}>
+                  <div className="cart-item__row">
+                    <div className="cart-item__image">
+                      <ImagePlaceholder image={image} name={name} />
+                    </div>
+                    <div className="cart-item__info">
+                      <p className="cart__product-title">{name}</p>
+                      <p className="cart__product-subtitle">Tam.: {size}</p>
+                      <div className="change-quantity">
+                        <button
+                          className="change-quantity__button"
+                          onClick={() => handleSubtractItem(selectedSku)}
+                        >
+                          -
+                        </button>
+                        <span className="change-quantity__current">
+                          {quantity}
+                        </span>
+                        <button
+                          className="change-quantity__button"
+                          onClick={() => handleAddItem(selectedSku)}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+                    <div className="cart-item__price">
+                      <p className="cart__product-title">R$ {actual_price}</p>
+                      <p className="cart__product-subtitle">{installments}</p>
+                    </div>
                   </div>
-                </div>
-                <div className="chosed-product__price">
-                  <p className="cart__product-title">R$ 99,90</p>
-                  <p className="cart__product-subtitle">3 x R$ 33,00</p>
-                </div>
-              </div>
-              <div className="chosed-product__row">
-                <button className="cart__remove">Remover item</button>
-              </div>
-            </article>
+                  <div className="cart-item__row">
+                    <button
+                      className="cart__remove"
+                      onClick={() => handleRemoveItem(selectedSku)}
+                    >
+                      Remover item
+                    </button>
+                  </div>
+                </article>
+              );
+            })}
           </div>
         </section>
       ) : (
