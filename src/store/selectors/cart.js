00a3e6eb3249408cbcf00d74c11333dbaba18651
Valenza <matheusvvalenza@gmail.com>
2020-06-06 12:31:42 -0300

    renamed reduce params

--- src/store/selectors/cart.js
+++ src/store/selectors/cart.js
@@ -6,15 +6,16 @@ const subTotalSelector = createSelector(
   (state) => state.cart.cartItems,
   (cartItems) =>
     cartItems.reduce(
-      (acc, cartItem) =>
-        acc + currencyToNumber(cartItem.actual_price) * cartItem.quantity,
+      (subTotal, cartItem) =>
+        subTotal + currencyToNumber(cartItem.actual_price) * cartItem.quantity,
       0
     )
 );
 
 const itemsQuantitySelector = createSelector(
   (state) => state.cart.cartItems,
-  (cartItems) => cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+  (cartItems) =>
+    cartItems.reduce((quantity, cartItem) => quantity + cartItem.quantity, 0)
 );
 
 export { subTotalSelector, itemsQuantitySelector };
