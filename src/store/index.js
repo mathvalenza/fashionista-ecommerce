29ad3c2364beb9a5afb48f6e7a0832e654d6cee6
Valenza <matheusvvalenza@gmail.com>
2020-06-06 11:27:17 -0300

    fixed redux usage on mobile

--- src/store/index.js
+++ src/store/index.js
@@ -9,12 +9,8 @@ const reducers = combineReducers({
   cart
 });
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
