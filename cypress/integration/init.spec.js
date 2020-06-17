7b6f6b5362532d26cd42916855cbf9348d1fe68c
Valenza <matheusvvalenza@gmail.com>
2020-06-16 21:51:15 -0300

    updated e2e tests

--- cypress/integration/init.spec.js
+++ cypress/integration/init.spec.js
@@ -19,22 +19,10 @@ describe('Products', () => {
       .should('have.length', 22);
   });
 
-  it('search list of products', () => {
-    cy.visit('/');
-
-    cy.get('[data-testid="search-icon"]').click();
-
-    cy.get('[data-testid="search-input"]').type('blusa');
-
-    cy.get('[data-testid="search-content"]')
-      .children()
-      .should('have.length', 2);
-  });
-
   it('add a product to the cart', () => {
     cy.visit('/');
 
-    cy.get('[data-testid="products-list"]').children().first().click();
+    cy.contains('BOW').click();
 
     cy.url().should('include', '/product/');
 
@@ -55,5 +43,17 @@ describe('Products', () => {
       .invoke('getState')
       .its('cart.cartItems')
       .should('have.length', 1);
+  });
+
+  it('search list of products', () => {
+    cy.visit('/');
+
+    cy.get('[data-testid="search-icon"]').click();
+
+    cy.get('[data-testid="search-input"]').type('blusa');
+
+    cy.get('[data-testid="search-content"]')
+      .children()
+      .should('have.length', 2);
   });
 });
